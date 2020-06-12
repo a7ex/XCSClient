c0538eb5a3dc01f1dc66129d696919275c1f55c4
Alex da Franca <alex.dafranca@dpdhl.com>
2020-06-12 15:36:23 +0200

    Initial Commit

---
+++ XCSClient/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  XCSClient
+//
+//  Created by Alex da Franca on 12.06.20.
+//  Copyright © 2020 Farbflash. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
