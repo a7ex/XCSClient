7d31ce3b86adf426c842aca1b39b4b87206d803d
Alex da Franca <alex.dafranca@dpdhl.com>
2021-03-25 08:54:21 +0000

    changed color for internal processing error, which appears randomly without any apparent reason to the color of a warning, as it is not really failed/red. This is an issue of the Xcode server itself.

--- XCSClient/Business/CoreData/CoreData+ViewModel/CDIntegration+ViewModel.swift
+++ XCSClient/Business/CoreData/CoreData+ViewModel/CDIntegration+ViewModel.swift
@@ -125,10 +125,12 @@ extension CDIntegration: IntegrationViewModel {
         switch integrationResult {
         case .analyzerWarnings, .warnings:
             return Color(Colors.warning)
-        case .buildErrors, .buildFailed, .checkoutError, .internalBuildError, .internalCheckoutError, .internalError, .internalProcessingError, .triggerError:
+        case .buildErrors, .buildFailed, .checkoutError, .internalBuildError, .internalCheckoutError, .internalError, .triggerError:
             return Color(Colors.error)
         case .canceled:
             return Color(Colors.canceled)
+        case .internalProcessingError:
+            return Color(Colors.warning)
         case .succeeded:
             return Color(Colors.success)
         case .testFailures:
